45a045efc2efca5a0d61f952fc57b67759ca7105
peace098beat <peace098beat.t@gmail.com>
2019-04-23 23:20:44 +0900

    add favicon

--- index.php
+++ index.php
@@ -41,6 +41,7 @@ $dif_days = (strtotime(date("Y-m-d", $dif)) - strtotime("1970-01-01")) / 86400;
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
     <title>いんべんとかれんだー</title>
     <!-- Bootstrap -->
     <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
